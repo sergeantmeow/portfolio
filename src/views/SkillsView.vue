c867d7cc9922b778124cac12d085e65546b42ebc
DAEHYUN AN <daehyunan@DAEHYUNui-MacBookAir.local>
2024-08-14 19:35:59 +0900

    added

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,37 +1,90 @@
 <template>
-  <div class="home">
-    <div id="wrapper" class="row">
-      <div id="home_face_photo_area" class="col-12 col-md-5 d-flex align-items-center justify-content-center">
-        <img id="home_face_photo" alt="" src= "../assets/memoji2.png">
-      </div>
-      <div id="home_message" class="col-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-start">
-        <HomeMessage />
-      </div>
+  <div class="profile">
+    <nav id="profile_navbar">
+      <span class="provile_nav">
+        <a href="#aboutMe" class="text-decoration-none m-2" @click.prevent="scrollTo('aboutMe', 100)">About Me</a>
+      </span>
+      <span class="profile_nav">
+        <a href="#skillView" class="text-decoration-none m-2" @click.prevent="scrollTo('skillView', 200)">Skills</a>
+      </span>
+      <span class="profile_nav">
+        <a href="#experienceView" class="text-decoration-none m-2" @click.prevent="scrollTo('experienceView', 300)">Experiences</a>
+      </span>
+      <span class="profile_nav">
+        <a href="#projectView" class="text-decoration-none m-2" @click.prevent="scrollTo('projectView', 400)">Projects</a>
+      </span>
+      <span class="profile_nav">
+        <!-- <span id="language-dropdown" class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Language
+          </a>
+          <ul id="language-menu" class="dropdown-menu">
+            <li><a class="dropdown-item fw-bold" href="#" @click.prevent = "setKorean"><img src="../assets/korflag.png" class="flagImg" alt="">한국어</a></li>
+            <li><a class="dropdown-item fw-bold" href="#" @click.prevent = "setEnglish"><img src="../assets/usflag.png" class="flagImg" alt="">English</a></li>
+          </ul>
+        </span> -->
+      </span>
+    </nav>
+    <div>
+      <AboutMe :language-set='languageSetting' id="aboutMe" class="view_element" />
+      <SkillView :language-set='languageSetting' id="skillView" class="view_element" />
+      <ExperienceView :language-set='languageSetting' id="experienceView" class="view_element" />
+      <ProjectView :language-set='languageSetting' id="projectView" class="view_element" />
     </div>
   </div>
 </template>
 
 <script>
-import HomeMessage from '@/components/HomeMessage.vue'
+import AboutMe from '@/components/AboutMe.vue'
+import SkillView from '@/components/SkillView.vue'
+import ExperienceView from '@/components/ExperienceView.vue'
+import ProjectView from '@/components/ProjectView.vue'
+import { smoothScroll } from '@/smooth-scroll.js';
 
 export default {
-  name: 'HomeView',
+  name : 'ProfileView',
+  props : {
+    languageSetting: {
+      type: String,
+      default : 'english'
+    },
+  },
   components: {
-    HomeMessage
+    AboutMe,
+    SkillView,
+    ExperienceView,
+    ProjectView,
+  },
+  methods: {
+    scrollTo(targetId, duration) {
+      smoothScroll(targetId, duration);
+    },
   }
 }
 </script>
-<style>
-  #wrapper{
-    display : flex;
-    width: 100%;
-  }
-  #home_face_photo {
-    margin-top : 5%;
-    width : 100%;
-  }
-  #home_message{
-    margin-top : 6px;
-    margin-bottom : 14px;
-  }
+
+<style scoped>
+.view_element{
+  margin : 14px;
+  border-radius : 6px;
+  background-color: rgb(32, 55, 95);
+}
+/* #language-dropdown{
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 6px;
+}
+#language-menu {
+  min-width : 120px;
+  margin-right : 10px;
+  background-color: rgb(94, 94, 110);
+} */
+#profile_navbar{
+  margin-top:6px;
+}
+.flagImg{
+  margin-right: 4px;
+}
 </style>
